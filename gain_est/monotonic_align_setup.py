896a3084ab8ff9b473f4df08514524a4038220da
shineamean <shineamean@snu.ac.kr>
2022-10-14 12:41:34 +0900

    221014

--- gain_est/monotonic_align_setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-  name = 'monotonic_align',
-  ext_modules = cythonize("functional/monotonic_align/core.pyx"),
-  include_dirs=[numpy.get_include()]
-)
