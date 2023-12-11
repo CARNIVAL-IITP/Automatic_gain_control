2c7cd5970f0a6766d28a34194d17a7cec8213dbb
yhjeon <yhjeon@hi.snu.ac.kr>
2023-12-11 08:47:22 +0000

    Add training code v2.1.2

---
+++ models/AGC_endtoend_r1/train.py
@@ -0,0 +1,35 @@
+import argparse
+from dataset import AGCDataset#, collate
+from solver import Solver
+from model import AGC_STFT_GRU
+import torch
+from torch.utils.data import DataLoader
+import config
+import os
+
+# os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3,4,mo"
+
+parser = config.parser
+
+def main(args):
+    tr_dataset = AGCDataset(json_path = '/home/yhjeon/projects/IITP_SE/NS_AGC/data/SITEC_AGC/train.json', sample_rate=args.sample_rate, wav_len=args.wav_len, win_len=args.win_len, hop_len=args.hop_len)
+    tr_dataloader = DataLoader(tr_dataset, batch_size = args.batch_size, shuffle=True, num_workers = args.num_workers)
+    cv_dataset = AGCDataset(json_path = '/home/yhjeon/projects/IITP_SE/NS_AGC/data/SITEC_AGC/valid.json', sample_rate=args.sample_rate, wav_len=args.wav_len, win_len=args.win_len, hop_len=args.hop_len)
+    cv_dataloader = DataLoader(cv_dataset, batch_size = args.batch_size, shuffle=True, num_workers = args.num_workers)
+
+    data = {'tr_loader': tr_dataloader, 'cv_loader': cv_dataloader}
+
+    model = AGC_STFT_GRU(160, 10, 160, 80)
+    if args.use_cuda:
+        model = torch.nn.DataParallel(model)
+        model.cuda()
+    
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 200, eta_min=1.0e-7)
+    solver = Solver(data, model, optimizer, scheduler, args)
+    solver.train()
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print(args)
+    main(args)
